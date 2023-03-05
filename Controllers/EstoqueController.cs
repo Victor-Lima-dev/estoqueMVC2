91e90d1bdfcc84dd64c1dcb243cc814abd06e7ef
VictorLima <victoraugustolima1@gmail.com>
2023-03-05 17:05:28 -0300

    procura por categoria no estoque adicionado #3

--- Controllers/EstoqueController.cs
+++ Controllers/EstoqueController.cs
@@ -66,6 +66,31 @@ namespace estoqueMVC2.Controllers
             return View("Index", estoque);
         }
 
+        //pesquisar por categoria
+        [HttpGet("PesquisarPorCategoria")]
+        public IActionResult PesquisarPorCategoria(string categoria)
+        {
+            var produtos = _context.Produtos.Where(p => p.Categoria == categoria).ToList();
+            var ItensEstoque = _context.ItensEstoque.ToList();
+            //vincular o produto ao item de estoque
+            foreach (var item in ItensEstoque)
+            {
+                item.Produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == item.ProdutoId);
+            }
+            var estoque = new ProdutoItemEstoqueViewModel
+            {
+                Produtos = produtos,
+                ItensEstoque = ItensEstoque
+            };
+
+            var totalItensEstoque = ItensEstoque.Sum(item => item.Quantidade);
+            ViewBag.TotalItensEstoque = totalItensEstoque;
+            return View("Index", estoque);
+        }
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
